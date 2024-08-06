206b0b3976b3836187d52a625ea9c70a22dbd835
Puneet <puneet@optumsoft.com>
2024-08-06 15:35:17 +0530

    Added basic reacflow with elk graph

--- src/App.js
+++ src/App.js
@@ -1,44 +1,122 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import ShapeFlow from './components/Shapes';
-import AddButton from './components/AddButton';
-import ReactDragNDrop from './components/ReactDragNdrop';
-import ExpandAndCollapse from './components/ExpandAndCollapse';
+import { initialNodes, initialEdges } from './components/ModelGraph/nodes-edges.js';
+import ELK from 'elkjs/lib/elk.bundled.js';
+import React, { useCallback, useLayoutEffect } from 'react';
+import {
+   ReactFlow,
+   ReactFlowProvider,
+   addEdge,
+   Panel,
+   useNodesState,
+   useEdgesState,
+   useReactFlow,
+} from '@xyflow/react';
+import '@xyflow/react/dist/style.css';
 
-class ErrorBoundary extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      error: null,
-      errorInfo: null,
-    };
-  }
+const elk = new ELK();
 
-  componentDidCatch(error, errorInfo) {
-    this.setState({
-      error,
-      errorInfo,
-    });
-  }
+// Elk has a *huge* amount of options to configure. To see everything you can
+// tweak check out:
+//
+// - https://www.eclipse.org/elk/reference/algorithms.html
+// - https://www.eclipse.org/elk/reference/options.html
+const elkOptions = {
+   'elk.algorithm': 'layered',
+   'elk.layered.spacing.nodeNodeBetweenLayers': '100',
+   'elk.spacing.nodeNode': '80',
+};
 
-  render() {
-    if (this.state.errorInfo) {
-      return <>Some thing wrong</>;
-    }
-    return this.props.children;
-  }
+const getLayoutedElements = (nodes, edges, options = {}) => {
+   const isHorizontal = options?.['elk.direction'] === 'RIGHT';
+   const graph = {
+      id: 'root',
+      layoutOptions: options,
+      children: nodes.map((node) => ({
+         ...node,
+         // Adjust the target and source handle positions based on the layout
+         // direction.
+         targetPosition: isHorizontal ? 'left' : 'top',
+         sourcePosition: isHorizontal ? 'right' : 'bottom',
+
+         // Hardcode a width and height for elk to use when layouting.
+         width: 150,
+         height: 50,
+      })),
+      edges: edges,
+   };
+
+   return elk
+      .layout(graph)
+      .then((layoutedGraph) => ({
+         nodes: layoutedGraph.children.map((node) => ({
+            ...node,
+            // React Flow expects a position property on the node instead of `x`
+            // and `y` fields.
+            position: { x: node.x, y: node.y },
+         })),
+
+         edges: layoutedGraph.edges,
+      }))
+      .catch(console.error);
+};
+
+function LayoutFlow() {
+   const [nodes, setNodes, onNodesChange] = useNodesState([]);
+   const [edges, setEdges, onEdgesChange] = useEdgesState([]);
+   const { fitView } = useReactFlow();
+
+   const onConnect = useCallback(
+      (params) => setEdges((eds) => addEdge(params, eds)),
+      [],
+   );
+   const onLayout = useCallback(
+      ({ direction, useInitialNodes = false }) => {
+         const opts = { 'elk.direction': direction, ...elkOptions };
+         const ns = useInitialNodes ? initialNodes : nodes;
+         const es = useInitialNodes ? initialEdges : edges;
+
+         getLayoutedElements(ns, es, opts).then(
+            ({ nodes: layoutedNodes, edges: layoutedEdges }) => {
+               setNodes(layoutedNodes);
+               setEdges(layoutedEdges);
+
+               window.requestAnimationFrame(() => fitView());
+            },
+         );
+      },
+      [nodes, edges],
+   );
+
+   // Calculate the initial layout on mount.
+   useLayoutEffect(() => {
+      onLayout({ direction: 'DOWN', useInitialNodes: true });
+   }, []);
+
+   return (
+      <div style={{ height: 800 }}>
+         <ReactFlow
+            nodes={nodes}
+            edges={edges}
+            onConnect={onConnect}
+            onNodesChange={onNodesChange}
+            onEdgesChange={onEdgesChange}
+            fitView
+         >
+            <Panel position="top-right">
+               <button onClick={() => onLayout({ direction: 'DOWN' })}>
+                  vertical layout
+               </button>
+
+               <button onClick={() => onLayout({ direction: 'RIGHT' })}>
+                  horizontal layout
+               </button>
+            </Panel>
+         </ReactFlow>
+      </div>
+   );
 }
 
-function App() {
-  return (
-    <ErrorBoundary>
-      <ExpandAndCollapse/>
-        {/* <ReactDragNDrop/> */}
-        {/* <ShapeFlow/> */}
-        {/* <AddButton/> */}
-    </ErrorBoundary>
-  );
-}
-
-export default App;
+export default () => (
+   <ReactFlowProvider>
+      <LayoutFlow />
+   </ReactFlowProvider>
+);
